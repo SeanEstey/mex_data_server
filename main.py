9a365842b3c9906c8e17a18caad104af6b107177
SeanEstey <estese@gmail.com>
2019-06-23 01:29:48 -0600

    chad trade alerts

--- main.py
+++ main.py
@@ -1,70 +1,90 @@
-from app.bitmex_websocket import BitMEXWebsocket
 import logging
+import json
 from time import sleep
+from datetime import datetime
+
+from app import *
+from app.gsheets import SS
+from app.bitmex_websocket import BitMEXWebsocket
+from app.timer import Timer
 from conf.mexapikey import api_key,api_secret
 from conf.gservkeys import gservacct
 from conf.conf import *
-from app.timer import Timer
 
-symbols=['XBTUSD','XBTM19','XBTU19','XBTZ19']
-
+#-------------------------------------------------------------------------------
+# TODO: find way to discover list of XBT fut symbols instead of hardcoding
+# TODO: find way to subcribe to multiple instruments using same websocket
 def run():
     logger = setup_logger()
-
     ws=[]
+
     for symbol in symbols:
-        ws.append(BitMEXWebsocket(
-            endpoint="wss://www.bitmex.com/realtime",
-            symbol=symbol, #"XBTUSD",
-            api_key=api_key,
-            api_secret=api_secret
-        ))
+        try:
+            ws.append(BitMEXWebsocket(
+                endpoint="wss://www.bitmex.com/realtime",
+                symbol=symbol,
+                api_key=api_key,
+                api_secret=api_secret
+            ))
+        except Exception as e:
+            logger.critical(str(e))
 
-    t=Timer(name="SS_Timer",expire="every 5 clock minutes utc")
+    ss_tmr=Timer(name="ss_tmr", expire="every 60 clock minutes utc", quiet=True)
+    prnt_tmr=Timer(name="prnt_tmr", expire="every 1 clock minutes utc", quiet=True)
 
-    # Run forever
+    # Main loop
     while(ws[0].ws.sock.connected):
-        # Update Gsheet at fixed timer intervals
-        if t.remain()==0:
-            t.reset()
-            instruments=[]
-            for _ws in ws:
+        instruments=[]
+        for _ws in ws:
+            if _ws.exited:
+                logger.critical("lost socket connection to "+_ws.symbol)
+            else:
                 instruments.append(_ws.get_instrument())
-            append_oi(instruments)
+
+        print_chad(ws[0].recent_trades())
+
+        # Print instrument data to console
+        if prnt_tmr.remain()==0:
+            prnt_tmr.reset()
+            logger.info('Symbol:{0} Price:${1:,} Funding:{2}% OpenInterest:${3:,}'.format(
+                instruments[0]['underlying'],
+                int(instruments[0]['lastPrice']),
+                instruments[0]['fundingRate']*100,
+                sum([x['openInterest'] for x in instruments])
+            ))
+
+        # Append data to gsheet
+        if ss_tmr.remain()==0:
+            ss_tmr.reset()
+            write_gsheet(instruments)
         sleep(10)
 
-def setup_logger():
-    # Prints logger info to terminal
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    ch = logging.StreamHandler()
-    # create formatter
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    # add formatter to ch
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-    return logger
+    logger.critical("lost socket connection. exiting...")
 
-def append_oi(instruments):
-    from datetime import datetime
-    from app.gsheets import SS
+#-------------------------------------------------------------------------------
+def print_chad(trades):
+    chads=[x for x in trades if x['size'] > 1000000]
+    for chad in chads:
+        verb='bought' if chad['side']=='Buy' else 'sold'
+        print("Chad Trade: {0:,} contracts market {1}.".format(chad['size'],verb))
+    if len(chads)==0:
+        biggest=sorted(trades, key=lambda k: k['size'])[-1]
+        print("{0:,} non-chad trades, {1:,} contracts largest {2}.".format(len(trades),biggest['size'],biggest['side']))
 
-    total_oi=0
-    for instrument in instruments:
-        total_oi+=instrument['openInterest']
-
+#-------------------------------------------------------------------------------
+def write_gsheet(instruments):
     ss = SS(gservacct['oauth'], ss_id)
     wks = ss.wks(wks_name)
     row=[
-        str(datetime.now()),            # Date
-        "XBT",                          # Asset
-        "Bitmex",                       # Exchange
-        instruments[0]['lastPrice'],    # Price
-        "",                             # Perp Swap OI
-        "",                             # Futures OI
-        int(total_oi/100000)            # Total OI
+        str(datetime.utcnow()),                                                 # Date
+        instruments[0]["underlying"],                                           # Asset
+        instruments[0]['reference'],                                            # Exchange
+        instruments[0]['lastPrice'],                                            # PerpSwap Price
+        sum([x['fundingRate'] for x in instruments if x['fundingRate']]),       # PerpSwap Funding Rate
+        sum([x['openInterest'] for x in instruments if x['fundingRate']]),      # PerpSwap OI
+        sum([x['openInterest'] for x in instruments if not x['fundingRate']]),  # Futures OI sum
+        int(sum([x['openInterest'] for x in instruments])/100000)               # Total OI
     ]
-    print("Appending Open Interest: "+str(total_oi))
     wks.appendRows([row])
 
 
